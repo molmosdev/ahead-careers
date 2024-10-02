f17486411c85b447a3933b3639784d505d9e1a7e
molmos <molmosdev@gmail.com>
2024-10-02 22:20:26 +0200

    Refactor app component to import RouterOutlet and HeaderComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { Router, NavigationEnd, ActivatedRouteSnapshot } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRouteSnapshot, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
   selector: 'ac-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
