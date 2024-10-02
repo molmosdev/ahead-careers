91461310a5c2435d7100231e9b4a83b2a1e35334
molmos <molmosdev@gmail.com>
2024-10-02 22:18:14 +0200

    Add robots.txt and generate sitemap.xml
    - Add robots.txt file with user-agent and disallow rules
    - Generate sitemap.xml with homepage URL and metadata
    - Update server.ts to serve sitemap.xml
    - Update app.component.ts to set meta tags for each route
    - Update header.component.html to add lazy loading attribute to logo image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header.component';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRouteSnapshot } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'ac-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Ahead Careers';
+  constructor(
+    private meta: Meta,
+    private projectTitle: Title,
+    private router: Router
+  ) {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      const currentRoute = this.router.routerState.snapshot.root;
+      this.setMetaTagsForRoute(currentRoute);
+    });
+  }
+
+  setMetaTagsForRoute(route: ActivatedRouteSnapshot) {
+    const title = route.data['title'] || 'Ahead Careers - Tu camino hacia el éxito';
+    const description = route.data['description'] || 'Explora oportunidades laborales y consejos de carrera';
+    const keywords = route.data['keywords'] || 'Ahead Careers, oportunidades laborales, consejos de carrera';
+
+    this.projectTitle.setTitle(title);
+    this.meta.updateTag({ name: 'description', content: description });
+    this.meta.updateTag({ name: 'keywords', content: keywords });
+  }
 }
