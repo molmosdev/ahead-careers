867391a650a3f2c726f6515cc69ba9659dad056c
molmos <molmosdev@gmail.com>
2024-10-06 13:35:02 +0200

    Refactor AppComponent to use router events for setting meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRouteSnapshot, RouterOutlet } from '@angular/router';
-import { filter, firstValueFrom } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
@@ -12,23 +12,17 @@ import { HeaderComponent } from './core/components/header/header.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title: string = '';
-
   constructor(
     private meta: Meta,
     private projectTitle: Title,
     private router: Router
   ) {
-    this.manageRoutesChanges();
-  }
-
-  async manageRoutesChanges() {
-    await firstValueFrom(this.router.events.pipe(filter(event => event instanceof NavigationEnd)));
-    this.setMetaTagsForRoute(this.router.routerState.snapshot.root);
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      this.setMetaTagsForRoute(this.router.routerState.snapshot.root);
+    });
   }
 
   setMetaTagsForRoute(route: ActivatedRouteSnapshot) {
-    this.title = route.data['title'];
     this.projectTitle.setTitle(route.data['title']);
     this.meta.updateTag({ name: 'description', content: route.data['description'] });
     this.meta.updateTag({ name: 'keywords', content: route.data['keywords'] });
