641e246c7b586d563969030094b52565c46ed4ca
molmos <molmosdev@gmail.com>
2024-10-08 20:53:23 +0200

    Refactor CSS to adjust sticky header positioning, add padding to experiences component, and set initial opacity to carousel and value boxes components

--- src/app/shared/directives/fade-in-on-enter-view.directive.ts
+++ src/app/shared/directives/fade-in-on-enter-view.directive.ts
@@ -11,6 +11,10 @@ export class FadeInOnEnterViewDirective implements OnInit, OnDestroy {
   constructor(private el: ElementRef) {}
 
   ngOnInit() {
+    // Set the initial opacity to 0
+    this.el.nativeElement.style.opacity = '0';
+
+    // Start the fade-in animation when the element enters the
     inView(this.el.nativeElement, () => {
       this.fadeAnimation = animate(this.el.nativeElement, { opacity: [0, 1] }, { duration: 1, delay: 0.2 });
       return () => this.fadeAnimation?.stop();
