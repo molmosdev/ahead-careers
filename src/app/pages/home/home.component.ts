c05dd6ec86a867311d1e552f4e2cbe97a088a5b8
molmos <molmosdev@gmail.com>
2024-10-08 21:33:25 +0200

    Refactor CSS to remove motion library and fade-in-on-enter-view directive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { ValuesComponent } from './components/values/values.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
-import { FadeInOnEnterViewDirective } from '../../shared/directives/fade-in-on-enter-view.directive';
+import { fadeInOutTrigger } from '../../shared/animations';
 
 @Component({
   selector: 'ac-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [ValuesComponent, ExperiencesComponent, FadeInOnEnterViewDirective],
+  imports: [ValuesComponent, ExperiencesComponent],
+  animations: [fadeInOutTrigger],
 })
 export class HomeComponent {
   constructor() {}
--- src/app/shared/directives/fade-in-on-enter-view.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FadeInOnEnterViewDirective } from './fade-in-on-enter-view.directive';
-
-describe('FadeInOnEnterViewDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FadeInOnEnterViewDirective();
-    expect(directive).toBeTruthy();
-  });
-});
