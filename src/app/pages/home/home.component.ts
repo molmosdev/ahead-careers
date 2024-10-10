5c7bc299b58aaf54edcf8d50b8c81b6ec4d2d1e2
molmos <molmos@circontrol.com>
2024-10-10 10:57:08 +0200

    feat: get description data from sanity

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ValuesComponent } from './components/values/values.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
 import { PlaceholderComponent } from '../../shared/components/placeholder/placeholder.component';
+import { SanityService } from '../../core/services/sanity.service';
+import { fadeInOutTrigger } from '../../shared/animations';
 
 @Component({
   selector: 'ac-home',
@@ -9,7 +11,17 @@ import { PlaceholderComponent } from '../../shared/components/placeholder/placeh
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   imports: [ValuesComponent, ExperiencesComponent, PlaceholderComponent],
+  animations: [fadeInOutTrigger],
 })
 export class HomeComponent {
-  constructor() {}
+  description = signal<string | undefined>(undefined);
+  constructor(private sanityService: SanityService) {
+    this.getDescription();
+  }
+
+  async getDescription() {
+    this.sanityService.getDataByType('businessDescription').then(data => {
+      this.description.set(this.sanityService.transformBlockToHtml(data[0].description));
+    });
+  }
 }
