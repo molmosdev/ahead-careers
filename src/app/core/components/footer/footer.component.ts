6436577285228112481dd23b7af6039a3f11a2b2
molmos <molmosdev@gmail.com>
2024-10-13 14:56:59 +0200

    Refactor footer component HTML to add LinkedIn icon

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { PlaceholderComponent } from '../../../shared/components/placeholder/placeholder.component';
+import { fadeInOutTrigger } from '../../../shared/animations';
 
 @Component({
   selector: 'ac-footer',
   standalone: true,
-  imports: [],
+  imports: [PlaceholderComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
+  animations: [fadeInOutTrigger],
 })
 export class FooterComponent {}
