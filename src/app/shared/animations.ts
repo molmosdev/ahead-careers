55e8a5c1937de976db3fe33cb02969e0ebbe10a0
molmos <molmosdev@gmail.com>
2024-10-13 15:05:56 +0200

    Refactor HTML templates to use @fadeInOut animation trigger

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -1,20 +1,10 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, AnimationTriggerMetadata, style, transition, trigger } from '@angular/animations';
 
 /**
- * The conditional opacity animation trigger
+ * The fade in and out animation trigger with delay parameter
  * @returns {AnimationTriggerMetadata} - The animation trigger metadata
  */
-export const opacityTrigger = trigger('opacityTrigger', [
-  state('true', style({ opacity: 1 })),
-  state('false', style({ opacity: 0 })),
-  transition('true <=> false', [animate('0.2s')]),
-]);
-
-/**
- * The fade in and out animation trigger with parameters
- * @returns {AnimationTriggerMetadata} - The animation trigger metadata
- */
-export const fadeInOutTrigger = trigger('fadeInOutTrigger', [
+export const fadeInOutTrigger: AnimationTriggerMetadata = trigger('fadeInOut', [
   transition(':enter', [style({ opacity: 0 }), animate('0.5s {{ delay }}', style({ opacity: 1 }))], {
     params: { delay: '0.2s' },
   }),
