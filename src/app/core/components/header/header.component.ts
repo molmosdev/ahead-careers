87a16186326d4ad328606e209182aed4325606d3
molmos <molmosdev@gmail.com>
2024-12-10 21:50:24 +0100

    Enhance header component with dynamic routing and improve footer styling

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component, HostListener, signal } from '@angular/core';
 import { fadeInOutTrigger } from '../../../shared/animations';
 import { moveToTheLeftTrigger } from './header.animations';
 import { Button } from '@realm-ui/angular';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ac-header',
@@ -17,5 +17,23 @@ export class HeaderComponent {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.scrolled.set(document.documentElement.scrollTop > 200);
+  }
+
+  constructor(protected route: ActivatedRoute) {}
+
+  /**
+   * Gets the current page title from the route data.
+   * @returns The title of the current page
+   */
+  getCurrentPageTitle(): string {
+    let title = '';
+    this.route.firstChild?.data.subscribe(data => {
+      title = data['title'];
+    });
+    return title;
+  }
+
+  isOffersPage(): boolean {
+    return this.getCurrentPageTitle() === 'Ofertas - Ahead Careers';
   }
 }
