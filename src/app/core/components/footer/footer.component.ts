df8bf85b1511f7885affc6f35d7659ec4b54acab
molmos <molmosdev@gmail.com>
2024-12-12 20:33:24 +0100

    Refactor footer component to simplify HTML structure and remove unused placeholder import

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { PlaceholderComponent } from '../../../shared/components/placeholder/placeholder.component';
 import { fadeInOutTrigger } from '../../../shared/animations';
 
 @Component({
   selector: 'ac-footer',
-  imports: [PlaceholderComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
   animations: [fadeInOutTrigger],
