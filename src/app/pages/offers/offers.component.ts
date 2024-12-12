37faa031ff9e0c6f6436750675cbf05748c98fd8
molmos <molmosdev@gmail.com>
2024-12-12 22:38:47 +0100

    Enhance offers component with deferred rendering and fade-in animation; adjust CSS for minimum height

--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -1,15 +1,17 @@
 import { Component, computed } from '@angular/core';
 import { SanityService } from '../../core/services/sanity.service';
 import { Offer } from './interfaces/offer.interface';
+import { fadeInOutTrigger } from '../../shared/animations';
 
 @Component({
   selector: 'ac-offers',
   imports: [],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css',
+  animations: [fadeInOutTrigger],
 })
 export class OffersComponent {
-  offers = computed<Offer[]>(() => this.sanityService.data.value().offers);
+  offers = computed<Offer[]>(() => this.sanityService.data.value()?.offers);
 
   constructor(private sanityService: SanityService) {
     this.sanityService.params.set({ type: 'offers', singleton: true });
