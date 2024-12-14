933c9a64418505d7aeb60d29cb413ad6738b23e2
molmos <molmosdev@gmail.com>
2024-12-14 13:22:29 +0100

    Add fade-in/out animation to offers component; apply animation to offer items

--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -2,12 +2,14 @@ import { Component, computed, inject } from '@angular/core';
 import { SanityService } from '../../core/services/sanity.service';
 import { Offer } from './interfaces/offer.interface';
 import { RouterLink } from '@angular/router';
+import { fadeInOutTrigger } from '../../shared/animations';
 
 @Component({
   selector: 'ac-offers',
   imports: [RouterLink],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css',
+  animations: [fadeInOutTrigger],
 })
 export class OffersComponent {
   sanityService = inject(SanityService);
