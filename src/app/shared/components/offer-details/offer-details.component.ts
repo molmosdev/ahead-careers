510b2f649e7498d8336f0c80362e953b12bea56a
molmos <molmosdev@gmail.com>
2024-12-14 13:20:16 +0100

    Add fade-in/out animation to offer details component; wrap offer JSON output in a div

--- src/app/shared/components/offer-details/offer-details.component.ts
+++ src/app/shared/components/offer-details/offer-details.component.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
 import { Offer } from '../../../pages/offers/interfaces/offer.interface';
 import { JsonPipe } from '@angular/common';
+import { fadeInOutTrigger } from '../../animations';
 
 @Component({
   selector: 'ac-offer-details',
   imports: [JsonPipe],
   templateUrl: './offer-details.component.html',
   styleUrl: './offer-details.component.css',
+  animations: [fadeInOutTrigger],
 })
 export class OfferDetailsComponent {
   offer = input<Offer>();
