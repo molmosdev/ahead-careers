6612015c3e777279d67a0978ec1b337f8dacdf7b
molmos <molmosdev@gmail.com>
2024-12-16 21:28:02 +0100

    Add list mode support to offer details component for improved layout flexibility

--- src/app/shared/components/offer-details/offer-details.component.ts
+++ src/app/shared/components/offer-details/offer-details.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, input } from '@angular/core';
 import { Offer } from '../../../pages/offers/interfaces/offer.interface';
 import { fadeInOutTrigger } from '../../animations';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 import { SanityService } from '../../../core/services/sanity.service';
 import { Button } from '@realm-ui/angular';
 import { SocialButtonsComponent } from './components/social-buttons/social-buttons.component';
 
 @Component({
   selector: 'ac-offer-details',
-  imports: [DatePipe, Button, SocialButtonsComponent],
+  imports: [DatePipe, Button, SocialButtonsComponent, NgClass],
   templateUrl: './offer-details.component.html',
   styleUrl: './offer-details.component.css',
   animations: [fadeInOutTrigger],
@@ -16,6 +16,7 @@ import { SocialButtonsComponent } from './components/social-buttons/social-butto
 export class OfferDetailsComponent {
   offer = input.required<Offer>();
   sanityService = inject(SanityService);
+  listMode = input<boolean>(false);
 
   /**
    * Gets the html from a block
