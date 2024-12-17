49f0e73e8a5a9d49bb43790da85feefbb9dbd54c
molmos <molmosdev@gmail.com>
2024-12-17 21:07:06 +0100

    Update header animation to use calculated translation for improved responsiveness

--- src/app/core/components/header/header.animations.ts
+++ src/app/core/components/header/header.animations.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/animations';
 
 export const moveToTheLeftTrigger = trigger('moveToTheLeftTrigger', [
-  state('true', style({ transform: 'translateX(-85%)' })),
+  state('true', style({ transform: 'translateX(calc((100% + 3rem) * -1))' })),
   state('false', style({ transform: 'translateX(0)' })),
   transition('true <=> false', [animate('0.2s 0.1s')]),
   transition('false <=> false', [animate('0.2s')]),
