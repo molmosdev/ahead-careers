ef7165a34f1a29925a9718a6fe3dd554418dcd76
molmos <molmosdev@gmail.com>
2024-12-27 21:44:00 +0100

    Refactor footer component: remove negative classes, add privacy policy link, and improve layout

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { fadeInOutTrigger } from '../../../shared/animations';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ac-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
+  imports: [RouterLink],
   animations: [fadeInOutTrigger],
 })
 export class FooterComponent {}
