f585237bc1eabcbffb3454c3839bf71c15e0fb42
molmos <molmos@circontrol.com>
2025-01-08 00:47:21 +0100

    Refactor legal notice and privacy policy components to use signals for state management and improve data fetching

--- src/app/pages/legal-notice/legal-notice.component.ts
+++ src/app/pages/legal-notice/legal-notice.component.ts
@@ -1,33 +1,25 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { SanityService } from '../../core/services/sanity.service';
 import { ResponsiveService } from '../../core/services/responsive.service';
 import { TitleAndContent } from '../../shared/interfaces/title-and-content';
-import { fadeInOutTrigger } from '../../shared/animations';
 
 @Component({
   selector: 'ac-legal-notice',
   imports: [],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.css',
-  animations: [fadeInOutTrigger],
 })
 export class LegalNoticeComponent implements OnInit {
   sanityService = inject(SanityService);
-  legalNotice = computed<TitleAndContent>(() => {
-    const data = this.sanityService.data.value();
-    return {
-      ...data,
-      content: this.sanityService.transformBlockToHtml(data?.content),
-    };
-  });
+  legalNotice = signal<TitleAndContent | undefined>(undefined);
   responsiveService = inject(ResponsiveService);
   isMobile = computed(() => this.responsiveService.isMobile());
 
-  ngOnInit(): void {
-    this.sanityService.params.set({
-      type: 'legalNotice',
-      singleton: true,
-      filters: [],
+  async ngOnInit(): Promise<void> {
+    const data = await this.sanityService.getDataByType('legalNotice', true);
+    this.legalNotice.set({
+      ...data,
+      content: this.sanityService.transformBlockToHtml(data?.content),
     });
   }
 }
--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,33 +1,25 @@
-import { Component, computed, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { SanityService } from '../../core/services/sanity.service';
 import { ResponsiveService } from '../../core/services/responsive.service';
 import { TitleAndContent } from '../../shared/interfaces/title-and-content';
-import { fadeInOutTrigger } from '../../shared/animations';
 
 @Component({
   selector: 'ac-privacy-policy',
   imports: [],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css',
-  animations: [fadeInOutTrigger],
 })
 export class PrivacyPolicyComponent implements OnInit {
   sanityService = inject(SanityService);
-  privacyPolicy = computed<TitleAndContent>(() => {
-    const data = this.sanityService.data.value();
-    return {
-      ...data,
-      content: this.sanityService.transformBlockToHtml(data?.content),
-    };
-  });
+  privacyPolicy = signal<TitleAndContent | undefined>(undefined);
   responsiveService = inject(ResponsiveService);
   isMobile = computed(() => this.responsiveService.isMobile());
 
-  ngOnInit(): void {
-    this.sanityService.params.set({
-      type: 'privacyPolicy',
-      singleton: true,
-      filters: [],
+  async ngOnInit(): Promise<void> {
+    const data = await this.sanityService.getDataByType('privacyPolicy', true);
+    this.privacyPolicy.set({
+      ...data,
+      content: this.sanityService.transformBlockToHtml(data?.content),
     });
   }
 }
